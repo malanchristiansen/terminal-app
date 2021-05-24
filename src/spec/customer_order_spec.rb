19d89127e31fe5a2ef20e66af44ed1cd78b4f7a5
malanchristiansen <malanschristiansen@gmail.com>
2021-05-24 16:56:39 +1000

    created customer order and passed all test cases

--- src/spec/customer_order_spec.rb
+++ src/spec/customer_order_spec.rb
@@ -7,31 +7,36 @@
 # 6. should be able to choose the quantity
 # 7. should be able to retrieve the final bill
 
+require_relative "../customer_order.rb"
+
 describe CustomerOrder do 
     before(:each) do 
         @customer = CustomerOrder.new("John")
+    end 
     it "instance must have a readable name" do 
         expect(@customer.name).to eq("John")
     end 
     it "displays welcome message" do 
         msg = "Welcome to this craft beer menu, choose a delicious beer from the menu or exit the menu"
-        expect(@customer.welcome_msg).to eq(msg) 
+        expect(@customer.welcome_msg).to eq(msg)
     end 
     it "display the menu" do 
-        menu = {"1. Bentspoke's Crankshaft IPA"=> 12, "2. Young Henry's New Towner Australian Pale Ale" => 8, "3. Philter Brewing's Red Session Ale" => 10} 
+        menu = {"1. Bentspoke's Crankshaft IPA" => 12, "2. Young Henry's New Towner Australian Pale Ale" => 8, "3. Philter Brewing's Red Session Ale" => 10} 
         expect(@customer.menu).to eq(menu)
     end 
     it "get price on beer based on user choice" do 
+        @customer.menu
         user_choice = 1 
-        expect(@customer.get_item_price(user_choice)).to eq(10)
+        expect(@customer.get_item_price(user_choice)).to eq(12)
     end 
     it "should get the quantity" do 
         expect(@customer.get_quantity).to be > 0
     end 
 
     it "should calculate the final bill" do 
+        @customer.menu
        user_choice = 1 
        final_bill = @customer.get_item_price(user_choice) * @customer.get_quantity
-        expect(@customer.bill).to eq(final_bill)
+        expect(@customer.calculate_bill).to eq(final_bill)
     end 
 end 
